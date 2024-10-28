d40de9374727fe1afed398a6f549a710985a92a2
AfrizaIlhamzakyAbadi <ilhamzakyabadi@gmail.com>
2024-10-28 14:58:27 +0700

    Add files via upload

--- exercise 1/Source.cpp
+++ exercise 1/Source.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+//#include <cstdlb>
 #include <iostream>
 using namespace std;
 int n;
@@ -12,11 +14,22 @@ int f(int x)
 		return x * f(x - 1);
 	}
 }
+int test_f()
+{
+#ifdef NDEBUG
+	cerr << "Tests run with NDEBUG defined (asserts compiled out)";
+	abort();
+#endif
+	assert(f(0) == 1);
+	assert(f(1) == 1);
+	assert(f(2) == 2);
+	assert(f(3) == 6);
+	assert(f(5) == 120);
+	cout << "All tests succeeded.\n";
+	return 0;
+}
 int main()
 {
-	cout << "FACTORIAL OPERATION\n" << "To find n!, n is a whole number, input the value of n.\n";
-	cout << "n = ";
-	cin >> n;
-	cout << "n! = " << f(n);
+	test_f();
 	return 0;
 }
