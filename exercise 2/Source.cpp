849c41fff3100948a46070b5a0f1aa6fb168d170
AfrizaIlhamzakyAbadi <ilhamzakyabadi@gmail.com>
2024-10-28 16:10:30 +0700

    Update Source.cpp

--- exercise 2/Source.cpp
+++ exercise 2/Source.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
 int a{}, b{};
-void sum(int a, int b)
+int sum(int a, int b)
 {
-	cout << a + b;
+	return a + b;
 }
-void product(int a, int b)
+int product(int a, int b)
 {
-	cout << a * b;
+	return a * b;
 }
 int main()
 {
-	cout << "To calculate the sum and product of integer a and b, input the following values.\n" << "a = ";
+	cout << "To calculate the sum and product of integers a and b, input the following values.\n" << "a = ";
 	cin >> a;
 	cout << "b = ";
 	cin >> b;
-	cout << a << " + " << b << " = ";
-	sum(a, b);
-	cout << '\n' << a << " x " << b << " = ";
-	product(a, b);
+	cout << a << " + " << b << " = " << sum (a, b) << '\n' << a << " x " << b << " = " << product(a, b);
 	return 0;
 }
